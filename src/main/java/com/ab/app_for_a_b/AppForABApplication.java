487c341a95cbaa5f00d2fc043337e9afd5b44580
kardinaldon <kardinalmail@gmail.com>
2021-09-24 14:35:57 +0300

    start

---
+++ src/main/java/com/ab/app_for_a_b/AppForABApplication.java
@@ -0,0 +1,13 @@
+package com.ab.app_for_a_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForABApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForABApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ab/app_for_a_b/AppForABApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.app_for_a_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppForABApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
